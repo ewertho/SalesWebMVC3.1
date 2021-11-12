bf9e3895c4bfcfa7efc78020a4d2a5acc552126d
ewert <ewert@DESKTOP-8AI3JUU>
2021-11-12 12:32:10 -0300

    Adicionar arquivos de projeto.

---
+++ SalesWebMVC3.1/Controllers/DepartamentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC3._1.Models;
+using System.Collections.Generic;
+
+namespace SalesWebMVC3._1.Controllers
+{
+    public class DepartamentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departament> list = new List<Departament>();
+            list.Add(new Departament { Id=1, Name="Eletronico" });
+            list.Add(new Departament { Id=2, Name="Fashion" });
+            return View(list);
+        }
+    }
+}
---
+++ SalesWebMVC3.1/Models/Departament.cs
@@ -0,0 +1,8 @@
+﻿namespace SalesWebMVC3._1.Models
+{
+    public class Departament
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
